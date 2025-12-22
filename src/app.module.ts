2cda35cb854c1669b2ae55587f5a9ec76f15026a
rtnkwami <nukukwami@outlook.com>
2025-12-22 20:11:14 +0000

    feat: bootstrap user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-    imports: [ProductsModule],
+    imports: [ProductsModule, UsersModule],
 })
 export class AppModule {}
