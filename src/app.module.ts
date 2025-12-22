c88e970d5eb5cf609306efc2018a5159543624dc
rtnkwami <nukukwami@outlook.com>
2025-12-22 21:09:00 +0000

    feat: add config module and env variable validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { validate } from './env.validation';
 
 @Module({
-    imports: [ProductsModule, UsersModule],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            validate,
+        }),
+        ProductsModule,
+        UsersModule,
+    ],
 })
 export class AppModule {}
---
+++ src/env.validation.ts
@@ -0,0 +1,26 @@
+import { plainToInstance } from 'class-transformer';
+import { IsNumber, IsString, Max, Min, validateSync } from 'class-validator';
+
+class EnvironmentVariables {
+    @IsNumber()
+    @Min(0)
+    @Max(65535)
+    PORT: number;
+
+    @IsString()
+    DATABASE_URL: string;
+}
+
+export function validate(config: Record<string, unknown>) {
+    const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+        enableImplicitConversion: true,
+    });
+    const errors = validateSync(validatedConfig, {
+        skipMissingProperties: false,
+    });
+
+    if (errors.length > 0) {
+        throw new Error(errors.toString());
+    }
+    return validatedConfig;
+}
