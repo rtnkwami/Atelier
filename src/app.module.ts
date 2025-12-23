19820ec993bfd39a1a9fbb79e7c996d860ba1212
rtnkwami <nukukwami@outlook.com>
2025-12-23 05:06:09 +0000

    feat: add path specific middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,27 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './env.validation';
 import { AuthMiddleware } from './middleware/auth.middleware';
+import { CartsModule } from './carts/carts.module';
 
 @Module({
     imports: [
-        ConfigModule.forRoot({
-            isGlobal: true,
-            validate,
-        }),
+        ConfigModule.forRoot({ isGlobal: true, validate }),
         ProductsModule,
         UsersModule,
+        CartsModule,
     ],
 })
 export class AppModule {
     configure(consumer: MiddlewareConsumer) {
-        consumer.apply(AuthMiddleware).forRoutes('users');
+        consumer
+            .apply(AuthMiddleware)
+            .exclude(
+                { path: '/products', method: RequestMethod.GET },
+                { path: '/products/:id', method: RequestMethod.GET },
+            )
+            .forRoutes('*');
     }
 }
