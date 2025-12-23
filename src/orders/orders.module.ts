6e4670fe5b00845f2141d618a5fdac15d25133f1
rtnkwami <nukukwami@outlook.com>
2025-12-23 08:09:55 +0000

    fix: add missing product and cart module injection

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { PrismaService } from 'src/prisma.service';
+import { ProductsModule } from 'src/products/products.module';
+import { CartsModule } from 'src/carts/carts.module';
 
 @Module({
+    imports: [ProductsModule, CartsModule],
     controllers: [OrdersController],
     providers: [OrdersService, PrismaService],
 })
