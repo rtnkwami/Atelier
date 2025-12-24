b5c86bedf07b16a7e3f0716844e5c39be214fa3e
rtnkwami <nukukwami@outlook.com>
2025-12-24 10:48:00 +0000

    refactor: use config service to get env variables

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from 'src/generated/prisma/client';
 import { Pool } from 'pg';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
-        const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    constructor(private configService: ConfigService) {
+        const connectionString = configService.get<string>('DATABASE_URL');
+        const pool = new Pool({ connectionString });
         const adapter = new PrismaPg(pool);
         super({ adapter });
     }
