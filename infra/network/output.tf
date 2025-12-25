c0d35c9206a72285de1abfa690f4194db5f4b12d
rtnkwami <nukukwami@outlook.com>
2025-12-25 03:39:34 +0000

    feat(infra): add network outputs

---
+++ infra/network/output.tf
@@ -0,0 +1,19 @@
+output "web_subnet_ids" {
+  description = "IDs of the public web subnets"
+  value       = [for subnet in aws_subnet.web_subnets : subnet.id]
+}
+
+output "app_subnet_ids" {
+  description = "IDs of the private app subnets"
+  value       = [for subnet in aws_subnet.app_subnets : subnet.id]
+}
+
+output "db_subnet_ids" {
+  description = "IDs of the private database subnets"
+  value       = [for subnet in aws_subnet.db_subnets : subnet.id]
+}
+
+output "reserved_subnet_ids" {
+  description = "IDs of the private reserved subnets"
+  value = [for subnet in aws_aws_subnet.reserved_subnets : subnet.id]
+}
