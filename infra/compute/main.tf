006b248aecade5bba3e8eea72c9dea53f25a18c3
rtnkwami <nukukwami@outlook.com>
2026-01-07 08:47:19 +0000

    feat(infra): add autoscaling to api on memory and cpu

--- infra/compute/main.tf
+++ infra/compute/main.tf
@@ -64,8 +64,8 @@ resource "aws_lb_target_group" "api_lb_target_group" {
 resource "aws_ecs_task_definition" "api_task" {
   family                   = "${var.resource_prefix}-api"
   network_mode             = "awsvpc"
-  cpu                      = 256
-  memory                   = 512
+  cpu                      = 512
+  memory                   = 1024
   requires_compatibilities = ["FARGATE"]
 
   runtime_platform {
@@ -129,3 +129,48 @@ resource "aws_ecs_service" "api_service" {
 
 # ------------ Configure Service Auto Scaling -----------------#
 
+resource "aws_appautoscaling_target" "api_service_autoscaling" {
+  min_capacity = 1
+  max_capacity = 5
+  resource_id = "service/${aws_ecs_cluster.cluster.name}/${aws_ecs_service.api_service.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace = "ecs"
+
+  tags = {
+    "Name"         = "${var.resource_prefix}-api-service-autoscaling-target"
+    "Project"      = var.project_name
+    "ResourceType" = "Compute"
+  }
+}
+
+resource "aws_appautoscaling_policy" "api_service_cpu_autoscaling_policy" {
+  name = "${aws_ecs_service.api_service.name}-cpu-autoscaling"
+  policy_type = "TargetTrackingScaling"
+  resource_id = aws_appautoscaling_target.api_service_autoscaling.id
+  scalable_dimension = aws_appautoscaling_target.api_service_autoscaling.scalable_dimension
+  service_namespace = aws_appautoscaling_target.api_service_autoscaling.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value = 50
+  }
+}
+
+resource "aws_appautoscaling_policy" "api_service_memory_autoscaling_policy" {
+  name = "${aws_ecs_service.api_service.name}-memory-autoscaling"
+  policy_type = "TargetTrackingScaling"
+  resource_id = aws_appautoscaling_target.api_service_autoscaling.resource_id
+  scalable_dimension = aws_appautoscaling_target.api_service_autoscaling.scalable_dimension
+  service_namespace = aws_appautoscaling_target.api_service_autoscaling.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+    
+    target_value = 70
+  }
+}
