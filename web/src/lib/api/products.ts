6ea3d83041e4fa4d64fa13114cd66ed7ec2746cc
rtnkwami <nukukwami@outlook.com>
2026-01-16 13:50:50 +0000

    feat(web): remove Nextjs issue causing docker build failure due to missing env vars

--- web/src/lib/api/products.ts
+++ web/src/lib/api/products.ts
@@ -1,6 +1,8 @@
+import { unstable_noStore as noStore } from "next/cache";
 import { ProductResponse } from "./types/products";
 
 export async function getProducts() {
+    noStore();
     const response = await fetch(`${ process.env.BACKEND_URL }/products`);
     if (!response.ok) {
         throw new Error('Failed to fetch products');
