c9b490d5e7d9836aea61de9c2fd190f521bd5dd0
rtnkwami <nukukwami@outlook.com>
2026-01-16 13:36:36 +0000

    feat(infra): add frontend ecs service name for ci/cd

--- infra/compute/output.tf
+++ infra/compute/output.tf
@@ -5,3 +5,7 @@ output "ecs_cluster_name" {
 output "api_service_name" {
   value = aws_ecs_service.api_service.name
 }
+
+output "web_service_name" {
+  value = aws_ecs_service.frontend_service.name
+}
--- infra/output.tf
+++ infra/output.tf
@@ -5,3 +5,7 @@ output "ecs_cluster_name" {
 output "api_service_name" {
   value = module.compute.api_service_name
 }
+
+output "web_service_name" {
+  value = module.compute.web_service_name
+}
