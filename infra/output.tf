684913137facac10c031bdd536aa850a7917b121
rtnkwami <nukukwami@outlook.com>
2026-01-19 15:11:41 +0000

    feat(infra): add output for frontend lb dns name

--- infra/output.tf
+++ infra/output.tf
@@ -9,3 +9,7 @@ output "api_service_name" {
 output "web_service_name" {
   value = aws_ecs_service.frontend_service.name
 }
+
+output "app_public_endpoint_dns_name" {
+  value = aws_lb.public_load_balancer.dns_name
+}
