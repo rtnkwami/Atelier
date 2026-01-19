1a2bf4564ea9a47eaa1b6a47e725186d359d53b6
rtnkwami <nukukwami@outlook.com>
2026-01-19 15:08:17 +0000

    chore(infra): add comments explaining the use of sslmode=no-verify in the db connection string

--- infra/main.tf
+++ infra/main.tf
@@ -25,5 +25,12 @@ data "aws_availability_zones" "available" {
 
 locals {
   availability_zones =  toset(slice(data.aws_availability_zones.available.names, 0, 3))
+
+  # sslmode=no-verify is used in the database url because the Prisma ORM expects ssl for database connections in production.
+  # Within this IaC, there is no ssl configured for database connections between the api tasks and the database
+  # adding sslmode=no-verify removes any encryption errors from Prisma ORM that comes along with this.
+  #
+  # In addition, due to the database being locked down via its security group, and by being in a private subnet, ssl was not configured
+  # for connections between it and the backend api tasks.
   database_url = "postgresql://${var.database_user}:${var.database_password}@${aws_rds_cluster.db_cluster.endpoint}/${var.database_name}?sslmode=no-verify"
 }
