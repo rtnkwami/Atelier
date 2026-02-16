0a7cbb841808874586f51ac6b95530df00faf727
rtnkwami <nukukwami@outlook.com>
2026-02-16 21:36:49 +0000

    feat(api): set up mikro orm

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './env.validation';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import dbConfig from './mikro-orm.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, validate })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, validate }),
+    MikroOrmModule.forRoot(dbConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/api/src/mikro-orm.config.ts
@@ -0,0 +1,14 @@
+import { Migrator } from "@mikro-orm/migrations";
+import { defineConfig } from "@mikro-orm/postgresql";
+
+export default defineConfig({
+  entities: ['./dist/entities'],
+  entitiesTs: ['./src/entities'],
+  migrations: {
+    path: './dist/migrations',
+    pathTs: './src/migrations',
+  },
+  extensions: [Migrator],
+  clientUrl: process.env.DATABASE_URL,
+  connect: false,
+});
