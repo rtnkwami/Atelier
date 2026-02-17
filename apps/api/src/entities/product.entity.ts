89b7944e4f5544bc1f88bea285bff81cd5b6767b
rtnkwami <nukukwami@outlook.com>
2026-02-17 08:24:55 +0000

    feat(api): add product reservation table

--- apps/api/src/entities/product.entity.ts
+++ apps/api/src/entities/product.entity.ts
@@ -1,17 +1,23 @@
 import {
+  Collection,
   DecimalType,
   Entity,
   JsonType,
+  OneToMany,
   PrimaryKey,
   Property,
 } from '@mikro-orm/core';
 import { BaseEntity } from './base.entity';
 import { randomUUID } from 'crypto';
+import { ReservationItem } from './reservation-item.entity';
 
 @Entity()
 export class Product extends BaseEntity<'description' | 'images'> {
   @PrimaryKey({ type: 'uuid' })
   id: string = randomUUID();
+
+  @OneToMany(() => ReservationItem, (item) => item.product)
+  reservations = new Collection<ReservationItem>(this);
 
   @Property()
   name: string;
