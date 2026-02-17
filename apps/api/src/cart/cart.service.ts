1a2b83d0d8715272789e94bf85ba7690c3b5d64f
rtnkwami <nukukwami@outlook.com>
2026-02-17 22:44:49 +0000

    feat(cart): add cart deduplication

--- apps/api/src/cart/cart.service.ts
+++ apps/api/src/cart/cart.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRedis } from '@nestjs-redis/client';
 import type { RedisClientType } from 'redis';
-import { CreateCart } from 'contracts';
+import { Cart } from 'contracts';
 
 @Injectable()
 export class CartService {
@@ -11,21 +11,40 @@ export class CartService {
   ) {}
   private readonly redisPrefix = 'cart';
 
-  private aggregateCart(data: CreateCart) {
-    const total = data.items.reduce(
+  private aggregateCart(cart: Cart) {
+    const total = cart.items.reduce(
       (sum, item) => sum + item.price * item.quantity,
       0,
     );
-    const itemCount = data.items.reduce(
+    const itemCount = cart.items.reduce(
       (count, item) => count + item.quantity,
       0,
     );
 
     return {
-      items: data.items,
+      items: cart.items,
       total,
       itemCount,
     };
+  }
+
+  private mergeIncomingWithCurrentCart(incoming: Cart, existing: Cart) {
+    const currentCart = new Map(existing.items.map((item) => [item.id, item]));
+
+    for (const incomingItem of incoming.items) {
+      const existingItem = currentCart.get(incomingItem.id);
+
+      if (!existingItem) {
+        currentCart.set(incomingItem.id, incomingItem);
+        continue;
+      }
+
+      currentCart.set(incomingItem.id, {
+        ...existingItem,
+        quantity: existingItem.quantity + incomingItem.quantity,
+      });
+    }
+    return Array.from(currentCart.values());
   }
 
   upsertCart(key: string, data: CreateCart) {
