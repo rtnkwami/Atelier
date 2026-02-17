f6b931eee94ef08a159bc9e120daf660b8bb5d23
rtnkwami <nukukwami@outlook.com>
2026-02-17 16:22:14 +0000

    feat(api): add auth middleware to api

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,19 +1,40 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './env.validation';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { InventoryModule } from './inventory/inventory.module';
+import { CartModule } from './cart/cart.module';
 import dbConfig from './mikro-orm.config';
+import { AuthMiddleware } from './middleware/auth.middleware';
+// import { RedisModule } from '@nestjs-redis/client';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, validate }),
     MikroOrmModule.forRoot(dbConfig),
+    // RedisModule.forRoot({
+    //   isGlobal: true,
+    //   options: {
+    //     url: process.env.REDIS_ENDPOINT || 'redis://localhost:6379',
+    //   },
+    // }),
     InventoryModule,
+    CartModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  private configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .exclude(
+        { path: 'inventory', method: RequestMethod.GET },
+        { path: 'inventory/:id', method: RequestMethod.GET },
+        { path: 'inventory/search', method: RequestMethod.GET },
+      )
+      .forRoutes('*');
+  }
+}
