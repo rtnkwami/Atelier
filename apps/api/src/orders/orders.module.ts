ba04548f6de7613969dcdec090e31ff30fd167b0
rtnkwami <nukukwami@outlook.com>
2026-02-19 07:35:49 +0000

    feat(api): add functionality to create orders

--- apps/api/src/orders/orders.module.ts
+++ apps/api/src/orders/orders.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { CartModule } from 'src/cart/cart.module';
+import { InventoryModule } from 'src/inventory/inventory.module';
 
 @Module({
-  imports: [CartModule],
+  imports: [CartModule, InventoryModule],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
--- apps/api/src/orders/orders.service.ts
+++ apps/api/src/orders/orders.service.ts
@@ -1,12 +1,18 @@
-import { EntityManager } from '@mikro-orm/postgresql';
+import { EntityManager, Transactional } from '@mikro-orm/postgresql';
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { CartService } from 'src/cart/cart.service';
+import { OrderItem } from 'src/entities/order-item.entity';
+import { Order } from 'src/entities/order.entity';
+import { Product } from 'src/entities/product.entity';
+import { User } from 'src/entities/user.entity';
+import { InventoryService } from 'src/inventory/inventory.service';
 
 @Injectable()
 export class OrdersService {
   constructor(
     private readonly em: EntityManager,
     private readonly cartService: CartService,
+    private readonly inventoryService: InventoryService,
   ) {}
 
   private async validateCart(userId: string) {
@@ -17,11 +23,41 @@ export class OrdersService {
         'Cart is empty. No products to place an order',
       );
     }
-    return { cart, userId };
+    return cart;
   }
 
-  createOrder(userId: string) {
-    return 'This action adds a new order';
+  @Transactional()
+  public async createOrder(userId: string) {
+    const cart = await this.validateCart(userId);
+    const cartTotal = cart.items.reduce(
+      (acc, item) => acc + item.price * item.quantity,
+      0,
+    );
+
+    const order = new Order();
+    order.total = cartTotal;
+    order.user = this.em.getReference(User, userId);
+    this.em.persist(order);
+
+    for (const item of cart.items) {
+      const orderItem = new OrderItem();
+      orderItem.price = item.price;
+      orderItem.quantity = item.quantity;
+      orderItem.product = this.em.getReference(Product, item.id);
+
+      order.items.add(orderItem);
+    }
+
+    const dto = {
+      orderId: order.id,
+      items: cart.items.map((item) => ({
+        id: item.id,
+        quantity: item.quantity,
+      })),
+    };
+    await this.inventoryService.reserveInventory(dto);
+
+    return { order };
   }
 
   // findAll() {
