ccc8014fe40c76d0ed7b8a13a0d8b795fac043bd
rtnkwami <nukukwami@outlook.com>
2026-02-19 20:47:24 +0000

    chore(api): remove unneeded old code

--- api/src/orders/dto/update-order.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsEnum, IsNotEmpty } from 'class-validator';
-import { OrderStatusEnum } from 'src/generated/prisma/enums';
-
-export class UpdateOrderDto {
-    @IsNotEmpty()
-    @IsEnum(OrderStatusEnum)
-    public readonly status: OrderStatusEnum;
-}
