442f5c5e3b2b5c861746d0947e0e315b5fa6bfd4
rtnkwami <nukukwami@outlook.com>
2026-02-27 18:45:48 +0000

    chore(infra): move ecs service definitions into dedicated file for better organization

---
+++ infra/ecs-cluster.tf
@@ -0,0 +1,196 @@
+resource "aws_ecs_cluster" "cluster" {
+  name = "${var.resource_prefix}-ecs-cluster"
+
+  tags = {
+    "Name"         = "${var.resource_prefix}-ecs-cluster"
+    "Project"      = var.project_name
+    "ResourceType" = "Compute"
+  }
+}
+
+# Frontend Service Configuration
+
+resource "aws_ecs_service" "frontend_service" {
+  name            = "${var.resource_prefix}-web-service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.frontend_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [for subnet in aws_subnet.app_subnets : subnet.id]
+    security_groups = [aws_security_group.frontend_security_group.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.frontend_target_group.arn
+    container_name   = "${var.resource_prefix}-web"
+    container_port   = 3000
+  }
+
+  tags = {
+    "Name"         = "${var.resource_prefix}-web-service"
+    "Project"      = var.project_name
+    "ResourceType" = "Compute"
+  }
+}
+
+resource "aws_appautoscaling_target" "frontend_service_autoscaling" {
+  min_capacity       = 1
+  max_capacity       = 5
+  resource_id        = "service/${aws_ecs_cluster.cluster.name}/${aws_ecs_service.frontend_service.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+
+  tags = {
+    "Name"         = "${var.resource_prefix}-web-service-autoscaling-target"
+    "Project"      = var.project_name
+    "ResourceType" = "Compute"
+  }
+}
+
+resource "aws_appautoscaling_policy" "frontend_service_cpu_autoscaling_policy" {
+  name               = "${aws_ecs_service.frontend_service.name}-cpu-autoscaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.frontend_service_autoscaling.id
+  scalable_dimension = aws_appautoscaling_target.frontend_service_autoscaling.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.frontend_service_autoscaling.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value = 50
+  }
+}
+
+resource "aws_appautoscaling_policy" "frontend_service_memory_autoscaling_policy" {
+  name               = "${aws_ecs_service.frontend_service.name}-memory-autoscaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.frontend_service_autoscaling.resource_id
+  scalable_dimension = aws_appautoscaling_target.frontend_service_autoscaling.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.frontend_service_autoscaling.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+
+    target_value = 70
+  }
+}
+
+# Backend Service Configuration
+
+resource "aws_ecs_service" "api_service" {
+  name            = "${var.resource_prefix}-api-service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.api_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [for subnet in aws_subnet.app_subnets : subnet.id]
+    security_groups = [aws_security_group.api_security_group.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.api_target_group.arn
+    container_name   = "${var.resource_prefix}-api"
+    container_port   = 5000
+
+  }
+
+  tags = {
+    "Name"         = "${var.resource_prefix}-api-service"
+    "Project"      = var.project_name
+    "ResourceType" = "Compute"
+  }
+}
+
+resource "aws_appautoscaling_target" "api_service_autoscaling" {
+  min_capacity       = 1
+  max_capacity       = 5
+  resource_id        = "service/${aws_ecs_cluster.cluster.name}/${aws_ecs_service.api_service.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+
+  tags = {
+    "Name"         = "${var.resource_prefix}-api-service-autoscaling-target"
+    "Project"      = var.project_name
+    "ResourceType" = "Compute"
+  }
+}
+
+resource "aws_appautoscaling_policy" "api_service_cpu_autoscaling_policy" {
+  name               = "${aws_ecs_service.api_service.name}-cpu-autoscaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.api_service_autoscaling.id
+  scalable_dimension = aws_appautoscaling_target.api_service_autoscaling.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.api_service_autoscaling.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value = 50
+  }
+}
+
+resource "aws_appautoscaling_policy" "api_service_memory_autoscaling_policy" {
+  name               = "${aws_ecs_service.api_service.name}-memory-autoscaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.api_service_autoscaling.resource_id
+  scalable_dimension = aws_appautoscaling_target.api_service_autoscaling.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.api_service_autoscaling.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+
+    target_value = 70
+  }
+}
+
+
+
+# ------ Required Task Execution Role for Logs ----------- #
+
+resource "aws_cloudwatch_log_group" "api_service_logs" {
+  name              = "/ecs/${var.resource_prefix}-api"
+  retention_in_days = 1
+}
+
+resource "aws_cloudwatch_log_group" "frontend_service_logs" {
+  name              = "/ecs/${var.resource_prefix}-web"
+  retention_in_days = 1
+}
+
+data "aws_iam_policy_document" "ecs_task_execution_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "ecs-task-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_assume_role.json
+}
+
+data "aws_iam_policy" "ecs_task_execution_policy" {
+  name = "AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = data.aws_iam_policy.ecs_task_execution_policy.arn
+}
