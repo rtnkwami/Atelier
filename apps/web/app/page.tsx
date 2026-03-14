5ea239982ff459941b70f1a6afa5152b85a236c5
rtnkwami <nukukwami@outlook.com>
2026-03-14 00:49:51 +0000

    test(pipeline): test if fixed pipeline permissions work

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -8,7 +8,6 @@ export default async function CatalogPage() {
         <p>There was an error displaying the Catalog</p>
       )
     }
-    // hi this is a test
   return (
     <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
       <Catalog catalog={ catalog }/>
