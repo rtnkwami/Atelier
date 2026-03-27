1e5dffa00c8e85ffca9c1d228ac69d59da428a6b
rtnkwami <nukukwami@outlook.com>
2026-03-27 12:22:52 +0000

    fix(api): fix product name collisions in seeder

--- apps/api/src/seeders/inventory.seeder.ts
+++ apps/api/src/seeders/inventory.seeder.ts
@@ -7,7 +7,7 @@ export class InventoryProductSeeder extends Seeder {
   run(em: EntityManager) {
     for (let index = 0; index < 50; index++) {
       em.create(Product, {
-        name: faker.commerce.product(),
+        name: faker.commerce.productName(),
         description: faker.commerce.productDescription(),
         price: Number(faker.commerce.price()),
         category: faker.commerce.department(),
