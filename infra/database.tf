02b965bf7b29b5ecca9848d432f7e1434779680b
rtnkwami <nukukwami@outlook.com>
2026-03-27 08:19:42 +0000

    switch from aurora to regular rds

--- infra/database.tf
+++ infra/database.tf
@@ -9,40 +9,20 @@ resource "aws_db_subnet_group" "db_cluster_subnet_group" {
   }
 }
 
-resource "aws_rds_cluster" "db_cluster" {
-  cluster_identifier     = "${var.resource_prefix}-db-cluster"
-  engine                 = "aurora-postgresql"
-  engine_version         = "17.7"
-  engine_mode            = "provisioned"
-  database_name          = data.aws_ssm_parameter.database_name.value
-  master_username        = data.aws_ssm_parameter.database_user.value
-  master_password        = data.aws_secretsmanager_secret_version.database_password.secret_string
-  db_subnet_group_name   = aws_db_subnet_group.db_cluster_subnet_group.name
-  vpc_security_group_ids = [aws_security_group.database_cluster_security_group.id]
-  skip_final_snapshot    = true
+resource "aws_db_instance" "db_primary" {
+  multi_az = true
 
-
-  serverlessv2_scaling_configuration {
-    max_capacity             = 1.0
-    min_capacity             = 0.0
-    seconds_until_auto_pause = 600
-  }
+  allocated_storage    = 20
+  instance_class       = "db.t4g.medium"
+  engine               = "postgresql"
+  engine_version       = "17.1"
+  db_name              = data.aws_ssm_parameter.database_name.value
+  password             = data.aws_secretsmanager_secret_version.database_password.secret_string
+  db_subnet_group_name = aws_db_subnet_group.db_cluster_subnet_group.name
+  skip_final_snapshot  = true
 
   tags = {
     "Name"         = "${var.resource_prefix}-db-cluster"
-    "Project"      = var.project_name
-    "ResourceType" = "Database"
-  }
-}
-
-resource "aws_rds_cluster_instance" "db_cluster_instance" {
-  cluster_identifier = aws_rds_cluster.db_cluster.id
-  instance_class     = "db.serverless"
-  engine             = aws_rds_cluster.db_cluster.engine
-  engine_version     = aws_rds_cluster.db_cluster.engine_version
-
-  tags = {
-    "Name"         = "${var.resource_prefix}-db-cluster instance"
     "Project"      = var.project_name
     "ResourceType" = "Database"
   }
