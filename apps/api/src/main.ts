4a9bdd6cc9eab42e773ce898bb4e12fa77cb8beb
rtnkwami <nukukwami@outlook.com>
2026-03-29 00:10:48 +0000

    feat(api): enable request draining on application shut down

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.useLogger(app.get(Logger));
+  app.enableShutdownHooks();
   await app.listen(process.env.PORT ?? 5000, '0.0.0.0');
 }
 void bootstrap();
