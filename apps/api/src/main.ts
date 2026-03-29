bc7972bdfae6ef28dee07859c703dcaf1357a8ff
rtnkwami <nukukwami@outlook.com>
2026-03-29 00:14:32 +0000

    chore(api): add spaces to main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,9 +5,13 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(helmet());
+
   app.useLogger(app.get(Logger));
+
   app.enableShutdownHooks();
+
   await app.listen(process.env.PORT ?? 5000, '0.0.0.0');
 }
 void bootstrap();
